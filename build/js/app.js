9a11ecbbacba909ae3e3677f7e0f9f69aab9a839
Franche020 <64061827+Franche020@users.noreply.github.com>
2022-11-21 20:22:41 +0100

    Falta codigo javascript

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,8 @@
-console.log('hola mundo');
+//deteccion de que opcion se selecciona y escritura en la variable
+
+// deteccion de pulsacion de boton submit
+
+// escribir valor seleccionado en id="rating-value"
+
+// ocultar tarjeta con invisible
+
