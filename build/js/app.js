9fc9f4471b8ce0b7cd92012455bc07d732856f9c
Franche020 <64061827+Franche020@users.noreply.github.com>
2022-11-22 09:06:34 +0100

    html+css+js finalizado

--- build/js/app.js
+++ build/js/app.js
@@ -1,8 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  iniciarApp();
+});
+
+function iniciarApp() {
+  ratingsF();
+  submit();
+}
+
+const ratingValue = document.querySelector("#rating-value");
+const ratings = document.querySelectorAll(".ratings li");
+const buttonSubmit = document.querySelector(".button-submit");
+
+
 //deteccion de que opcion se selecciona y escritura en la variable
+function ratingsF() {
+  ratings.forEach(function (enlaces) {
+    enlaces.addEventListener("click", (e) => {
+      // Borrar las clases de todos los elementos para que no se resalten varios
+      ratings.forEach((li) => li.classList.remove("selected-rating"));
+      // escribir valor seleccionado en id="rating-value"
+      ratingValue.innerHTML = e.target.textContent;
+      // Añadir la clase para resaltar
+      e.target.classList.add("selected-rating");
+    });
+  });
+}
 
-// deteccion de pulsacion de boton submit
+// deteccion de pulsacion de botón submit
 
-// escribir valor seleccionado en id="rating-value"
+function submit() {
+    const normalState = document.querySelector('.normal-state');
+    const thanksState = document.querySelector('.thanks-state');
 
-// ocultar tarjeta con invisible
+    buttonSubmit.addEventListener('click', function () {
+        // ocultar tarjeta con invisible
+        normalState.classList.add('invisible');
+        thanksState.classList.remove('invisible')
+    })
+}
+
 
