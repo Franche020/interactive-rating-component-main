dac6ce30eca90779f1341c0b08f32b26abac279b
Franche020 <64061827+Franche020@users.noreply.github.com>
2022-11-23 08:16:05 +0100

    JS Flexbox to center and px removed

--- src/js/app.js
+++ src/js/app.js
@@ -11,6 +11,7 @@ const ratingValue = document.querySelector("#rating-value");
 const ratings = document.querySelectorAll(".ratings li");
 const buttonSubmit = document.querySelector(".button-submit");
 
+let ratedValue = 0;
 
 //deteccion de que opcion se selecciona y escritura en la variable
 function ratingsF() {
@@ -19,6 +20,7 @@ function ratingsF() {
       // Borrar las clases de todos los elementos para que no se resalten varios
       ratings.forEach((li) => li.classList.remove("selected-rating"));
       // escribir valor seleccionado en id="rating-value"
+      ratedValue = e.target.textContent;
       ratingValue.innerHTML = e.target.textContent;
       // Añadir la clase para resaltar
       e.target.classList.add("selected-rating");
@@ -32,11 +34,18 @@ function submit() {
     const normalState = document.querySelector('.normal-state');
     const thanksState = document.querySelector('.thanks-state');
 
+ 
     buttonSubmit.addEventListener('click', function () {
+
+      if(ratedValue !== 0) {
+
         // ocultar tarjeta con invisible
         normalState.classList.add('invisible');
         thanksState.classList.remove('invisible')
+      } else {
+
+      }
+
     })
+    
 }
-
-
